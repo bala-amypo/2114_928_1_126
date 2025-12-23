dba1655024f9b32cdf8c3e369098e3e4ae7ee9d2
bala-amypo <bala@amypo.in>
2025-12-23 16:44:37 +0000

    User commit

--- src/main/java/com/example/demo/repository/CustomerProfileRepository.java
+++ src/main/java/com/example/demo/repository/CustomerProfileRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface CustomerProfileRepository extends JpaRepository<CustomerProfile, Long> {
-    Optional<CustomerProfile> findByCustomerId(String customerId);
+public interface CustomerProfileRepository
+        extends JpaRepository<CustomerProfile, Long> {
+
+    Optional<CustomerProfile> findById(Long id);
 }
