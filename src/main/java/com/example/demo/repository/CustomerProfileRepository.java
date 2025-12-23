cae90c2ee24372626553bd64598b2458dd5bf5e5
bala-amypo <bala@amypo.in>
2025-12-23 08:30:11 +0000

    User commit

--- src/main/java/com/example/demo/repository/CustomerProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.CustomerProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerProfileRepository
-        extends JpaRepository<CustomerProfile, Long> {
-}
--- src/main/java/com/example/demo/repository/PurchaseRecordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.PurchaseRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface PurchaseRecordRepository extends JpaRepository<PurchaseRecord, Long> {
-    List<PurchaseRecord> findByCustomerId(Long customerId);
-}
