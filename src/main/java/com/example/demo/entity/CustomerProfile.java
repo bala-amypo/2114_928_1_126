12ba5910ff7afca5fea70bf25b7a4de042e770db
bala-amypo <bala@amypo.in>
2025-12-23 10:33:10 +0000

    User commit

--- src/main/java/com/example/demo/entity/CustomerProfile.java
+++ src/main/java/com/example/demo/entity/CustomerProfile.java
@@ -12,18 +12,38 @@ public class CustomerProfile {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String username;     // for AuthService
+    private String password;     // for AuthService
     private String fullName;
     private String email;
     private String phone;
     private String currentTier;
+    private boolean active;      // for AuthService / CustomerProfileService
 
     // Getters & Setters
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getFullName() {
@@ -56,5 +76,13 @@ public class CustomerProfile {
 
     public void setCurrentTier(String currentTier) {
         this.currentTier = currentTier;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
     }
 }
